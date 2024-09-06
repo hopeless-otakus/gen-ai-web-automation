3cd632d1bd89727c5bafb7080d3b03be430b91b7
Blink29 <purukumar2905@gmail.com>
2024-09-06 20:33:16 +0530

    form creation

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-app.config['WTF_CSRF_ENABLED'] = False
+# app.config['WTF_CSRF_ENABLED'] = False
 
 @app.route('/')
 def hello_world():
