89c15f66a219bad6218dcecc176073f7614d3193
Blink29 <purukumar2905@gmail.com>
2024-09-06 10:06:00 +0530

    hugo form showing, backend endpoint issue auth

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,6 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS 
 
 app = Flask(__name__)
+CORS(app)
+app.config['WTF_CSRF_ENABLED'] = False
 
 @app.route('/')
 def hello_world():
@@ -13,7 +16,7 @@ def submit():
     image_url = request.fomr['image_url']
 
     print(f'Title: {title}, Image URL: {image_url}, Link: {link}')
-    return 'Submit'
+    return jsonify({"message": "Form submitted successfully!"}), 200
 
 if __name__ == '__main__':
     app.run()
